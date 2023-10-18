e91e3b4f78a177bc0353c4af4036b1dd71a51d3b
Gabriela <ariettigabriela@gmail.com>
2023-10-18 20:27:07 -0300

    h1 style

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- CSS del bootstrap  -->
+  <meta name="description" content="Ropa importada al mejor precio">
+  <meta name="author" content="Gaby Arietti - Diseño web - gráfico - UX">
+  <meta name="keywords" content="ropa, importada, domicilio, precio, economica, argentina">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <title>Document</title>
+  <link rel="stylesheet" href="./public/style.css">
+  <title>Ni idea Store</title>
 </head>
 
 <body>
-  <h1>Tienda de ropa</h1>
+  <h1 id="main">NI IDEA STORE</h1>
   <button type="submit"><a href="index.html">Inicio</a></button>
   <button type="submit"><a href="listar.php">Listar ropa</a></button>
   <button type="submit"><a href="agregar.html">Agregar ropa</a></button>
