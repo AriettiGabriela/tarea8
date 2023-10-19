138e0ff004465f2cd22a39b7b2f221728e5bd981
Gabriela <ariettigabriela@gmail.com>
2023-10-19 16:02:12 -0300

    commit 5

--- public/basedatos.php
+++ public/basedatos.php
@@ -34,10 +34,10 @@
 
                 // 4) el while recorre todos los registros y genera una CARD PARA CADA UNA
                 while ($reg = mysqli_fetch_array($datos)) { ?>
-                    <div class="card col-sm-12 col-md-6 col-lg-3">
-                        <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen']) ?>" alt="" height="100%" )>
+                    <div class="card">
+                        <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen']) ?>">
                         <a href="ver.php?id=<?php echo $reg['id']; ?>" class="card-body">
-                            <h3 class="card-title" style="width: 100%; font-size:25px;"><?php echo ucwords($reg['marca']) ?></h3>
+                            <h3 class="card-title"><?php echo ucwords($reg['marca']) ?></h3>
                             <span>$ <?php echo $reg['precio']; ?></span>
                         </a>
                     </div>
