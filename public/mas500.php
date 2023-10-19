e5ef7698153b43aea4abc01291dc27ac723b851b
Gabriela <ariettigabriela@gmail.com>
2023-10-19 15:33:37 -0300

    commit4

--- public/mas500.php
+++ public/mas500.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS del bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
 </head>
@@ -14,25 +13,12 @@
     <section>
         <div class="container">
             <div class="row">
-
-
                 <?php
-                // 1) Conexion
                 $conexion = mysqli_connect("127.0.0.1", "root", "");
                 mysqli_select_db($conexion, "tienda");
-
-                // 2) Preparar la orden SQL
-                // Sintaxis SQL SELECT
-                // SELECT * FROM nombre_tabla
-                // => Selecciona todos los campos de la siguiente tabla
-                // SELECT campos_tabla FROM nombre_tabla
-                // => Selecciona los siguientes campos de la siguiente tabla
-                $consulta = 'SELECT * FROM ropa';
-
-                // 3) Ejecutar la orden y obtenemos los registros
+                $consulta = 'SELECT * FROM ropa WHERE precio>500';
                 $datos = mysqli_query($conexion, $consulta);
 
-                // 4) el while recorre todos los registros y genera una CARD PARA CADA UNA
                 while ($reg = mysqli_fetch_array($datos)) { ?>
                     <div class="card col-sm-12 col-md-6 col-lg-3">
                         <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen']) ?>" alt="" height="100%" )>
@@ -47,7 +33,6 @@
             </div>
         </div>
     </section>
-    <!-- JavaScript del bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
